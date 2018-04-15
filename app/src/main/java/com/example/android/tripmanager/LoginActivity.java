0b85dbd6e74b31aa656b8307c405c4fb2573cf9b
Jules <jules.larue@outlook.fr>
2018-04-15 16:19:14 +0300

    SignUp activity and layout

--- app/src/main/java/com/example/android/tripmanager/LoginActivity.java
+++ app/src/main/java/com/example/android/tripmanager/LoginActivity.java
@@ -1,13 +1,27 @@
 package com.example.android.tripmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView mTvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mTvSignUp = findViewById(R.id.tv_no_account_yet);
+        mTvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signUpIntent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(signUpIntent);
+            }
+        });
     }
 }
